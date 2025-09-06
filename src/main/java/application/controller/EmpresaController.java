8b83c39e03a37f9d25b15ae31045c9135a94fe66
laurafiadone <laurafiadone@gmail.com>
2025-09-06 15:13:01 -0300

    se agrego swagger

--- src/main/java/application/controller/EmpresaController.java
+++ src/main/java/application/controller/EmpresaController.java
@@ -3,14 +3,17 @@ package application.controller;
 import domain.model.Empresa;
 import domain.ports.in.EmpresaUC;
 import dto.EmpresaDTO;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/empresas")
+@Tag(name = "Empresas", description = "Operaciones relacionadas con empresas")
 public class EmpresaController {
 
     private EmpresaUC empresaUC;
@@ -21,12 +24,14 @@ public class EmpresaController {
     }
 
     @GetMapping
+    @Operation(summary = "Listar todas las empresas", description = "Devuelve una lista de empresas registradas")
     public List<EmpresaDTO> listEmpresas() {
         return empresaUC.listEmpresas().stream().map(dto -> new EmpresaDTO(dto.getCuit(), dto.getRazon_social(), dto.getFechaAdhesion())).toList();
     }
 
     @PostMapping
     /*Insert New Empresa*/
+    @Operation(summary = "Agregar una Empresa", description = "Agrega una nueva empresa al sistema")
     public ResponseEntity<EmpresaDTO> insertEmpresa(@RequestBody EmpresaDTO in_empresa) {
         Empresa empresa_save = new Empresa(in_empresa.getCuit(), in_empresa.getRazon_social(), in_empresa.getFechaAdhesion());
         Empresa empresa_creada = empresaUC.insertEmpresa(empresa_save);
@@ -36,11 +41,13 @@ public class EmpresaController {
 
     @GetMapping("/last-month")
     /*empresas que hicieron transferencias el último mes*/
+    @Operation(summary = "Listar empresas ultimo mes", description = "Devuelve una lista de empresas que se adherieron el ultimo mes")
     public List<EmpresaDTO> getEmpresasLastMonth() {
         return empresaUC.getEmpresasByLastMonth().stream().map(dto -> new EmpresaDTO(dto.getCuit(), dto.getRazon_social(), dto.getFechaAdhesion())).toList();
     }
 
     @GetMapping("/by-id/{id}")
+    @Operation(summary = "Listar empresas por cuit", description = "Devuelve empresas segun un cuit")
     public ResponseEntity<EmpresaDTO> searchByCuit(@PathVariable Long id) {
         return empresaUC.getEmpresasByCuit(id).map(entity -> new EmpresaDTO(entity.getCuit(), entity.getRazon_social(), entity.getFechaAdhesion())).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
--- src/main/java/application/controller/TransferenciaController.java
+++ src/main/java/application/controller/TransferenciaController.java
@@ -3,15 +3,18 @@ package application.controller;
 import domain.model.Transferencia;
 import domain.ports.in.TransferenciaUC;
 import dto.TransferenciaDTO;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
 
 import java.util.List;
 
 
 @RestController
 @RequestMapping("/api/transferencias")
+@Tag(name = "Transferencias", description = "Operaciones relacionadas con transferencias")
 public class TransferenciaController {
 
     private TransferenciaUC transferenciaUC;
@@ -22,11 +25,13 @@ public class TransferenciaController {
     }
 
     @GetMapping
+    @Operation(summary = "Listar todas las Transferencias", description = "Devuelve una lista de transferencias registradas")
     public List<TransferenciaDTO> getTransferencias() {
         return transferenciaUC.getTransferencias().stream().map(entity -> new TransferenciaDTO(entity.getImporte(), entity.getEmpresa(), entity.getCuentaDebito(), entity.getCuentaCredito(), entity.getFecha())).toList();
     }
 
     @PostMapping
+    @Operation(summary = "Agregar una nueva transferencia", description = "Agrega al sistema una nueva transferencia")
     public ResponseEntity<TransferenciaDTO> insertTransferencia(@RequestBody TransferenciaDTO in_Transferencia) {
         Transferencia transferencia_save = new Transferencia(in_Transferencia.getImporte(), in_Transferencia.getEmpresa(), in_Transferencia.getCuentaDebito(), in_Transferencia.getCuentaCredito(), in_Transferencia.getFecha());
         Transferencia transferencia_creada = transferenciaUC.insertTransferencia(transferencia_save);
@@ -37,11 +42,13 @@ public class TransferenciaController {
 
     @GetMapping("/period/last-transferencias")
     /*Empresas que hicieron transferencias el ultimo mes*/
+    @Operation(summary = "Obtener Empresas y Transferencias", description = "Devuelve una lista las ultimas empresas que hicieron transferencias")
     public List<TransferenciaDTO> getLastTransferencias() {
         return transferenciaUC.getLastTransferencias().stream().map(entity -> new TransferenciaDTO(entity.getImporte(), entity.getEmpresa(), entity.getCuentaDebito(), entity.getCuentaCredito(), entity.getFecha())).toList();
     }
 
     @GetMapping("/by-cuit/{cuit:\\d+}")
+    @Operation(summary = "Listar transferencias por cuit de empresa", description = "Devuelve una lista de transferencias segun un cuit de empresa")
     public List<TransferenciaDTO> getTransferenciasByCuit(@PathVariable Long cuit) {
         return transferenciaUC.getTransferenciasByCuit(cuit).stream().map(entity -> new TransferenciaDTO(entity.getImporte(), entity.getEmpresa(), entity.getCuentaDebito(), entity.getCuentaCredito(), entity.getFecha())).toList();
     }
